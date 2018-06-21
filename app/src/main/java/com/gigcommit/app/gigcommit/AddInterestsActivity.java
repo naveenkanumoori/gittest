b653eb10dfc0f8d742e23b5e4df695fe01f8ab43
naveenkanumoori <naveenkanumoori@gmail.com>
2018-06-20 21:48:48 -0400

    Added Add Interests Activity Screen

---
+++ app/src/main/java/com/gigcommit/app/gigcommit/AddInterestsActivity.java
@@ -0,0 +1,13 @@
+package com.gigcommit.app.gigcommit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddInterestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_interests);
+    }
+}
